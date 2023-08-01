022a03394a9eb1582576e7dff2cf3ce9210d75dc
kvpvishnu11 <130116370+kvpvishnu11@users.noreply.github.com>
2023-08-01 16:23:34 -0400

    Update StudentManagementSystemApplication.java

--- NewSMS/src/main/java/com/myproj/spring/sms/StudentManagementSystemApplication.java
+++ NewSMS/src/main/java/com/myproj/spring/sms/StudentManagementSystemApplication.java
@@ -51,7 +51,7 @@ public class StudentManagementSystemApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("http://34.72.81.122")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true)
