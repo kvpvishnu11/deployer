5841f46d596ee70da4cbdc18b3d64dc66f0ddb59
kvpvishnu11 <130116370+kvpvishnu11@users.noreply.github.com>
2023-08-01 16:08:17 -0400

    Update StudentManagementSystemApplication.java

--- NewSMS/src/main/java/com/myproj/spring/sms/StudentManagementSystemApplication.java
+++ NewSMS/src/main/java/com/myproj/spring/sms/StudentManagementSystemApplication.java
@@ -20,7 +20,7 @@
 //			public void addCorsMappings(CorsRegistry registry) {
 //			//	registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
 //				//registry.addMapping("/**").allowedOrigins("http://34.72.136.253:80");
-//				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
+//				registry.addMapping("/**").allowedOrigins("http://34.72.81.122");
 //				 
 //			}
 //		};
