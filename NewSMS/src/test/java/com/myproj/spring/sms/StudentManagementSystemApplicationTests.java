23f1ce3b07a5defd1f7976d4f9944e8770891f27
kvpvishnu11 <kvpvishnu11@gmail.com>
2023-07-31 20:56:01 -0400

    new db test

--- NewSMS/src/test/java/com/myproj/spring/sms/StudentManagementSystemApplicationTests.java
+++ NewSMS/src/test/java/com/myproj/spring/sms/StudentManagementSystemApplicationTests.java
@@ -2,6 +2,7 @@ package com.myproj.spring.sms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
@@ -10,6 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class StudentManagementSystemApplicationTests {
 
+    @Value("${DB_URL}")
+    private String dbUrl;
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
+
     @Autowired
     private ApplicationContext applicationContext;
 
@@ -17,5 +27,10 @@ class StudentManagementSystemApplicationTests {
     void contextLoads() {
         // Verify that the Spring application context is not null
         assertNotNull(applicationContext);
+
+        // Print the database connection properties
+        System.out.println("DB_URL: " + dbUrl);
+        System.out.println("DB_USERNAME: " + dbUsername);
+        System.out.println("DB_PASSWORD: " + dbPassword);
     }
 }
