32a4ffd78337cd74a9b74851bfe9c367718f48ab
kvpvishnu11 <130116370+kvpvishnu11@users.noreply.github.com>
2023-07-31 20:38:11 -0400

    Update StudentManagementSystemApplicationTests.java

--- NewSMS/src/test/java/com/myproj/spring/sms/StudentManagementSystemApplicationTests.java
+++ NewSMS/src/test/java/com/myproj/spring/sms/StudentManagementSystemApplicationTests.java
@@ -2,7 +2,7 @@ package com.myproj.spring.sms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//
 @SpringBootTest
 class StudentManagementSystemApplicationTests {
 
