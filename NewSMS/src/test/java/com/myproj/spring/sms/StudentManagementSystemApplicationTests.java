bda3231f196606008203060b2a0ebce9a0a0e223
kvpvishnu11 <130116370+kvpvishnu11@users.noreply.github.com>
2023-07-31 20:50:01 -0400

    Update StudentManagementSystemApplicationTests.java

--- NewSMS/src/test/java/com/myproj/spring/sms/StudentManagementSystemApplicationTests.java
+++ NewSMS/src/test/java/com/myproj/spring/sms/StudentManagementSystemApplicationTests.java
@@ -1,13 +1,21 @@
 package com.myproj.spring.sms;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class StudentManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    void contextLoads() {
+        // Verify that the Spring application context is not null
+        assertNotNull(applicationContext);
+    }
 }
